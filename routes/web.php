96927742d5d93875abedbcb7b7c0aedeb71e231c
itmquyen <quyen27321@gmail.com>
2023-06-04 18:56:19 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('menus',[App\Http\Controllers\MenuController::class, 'index']);
